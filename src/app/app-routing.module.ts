58fa83dacb2276567ea64de918f9dedaa96e2c6e
Hazar Durmazoğlu <hexagon6449@gmail.com>
2021-03-29 16:39:56 +0300

    29.03 refactoring

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarDetailComponent } from './components/car/car-detail/car-detail.component';
+import { CarFilterComponent } from './components/car/car-filter/car-filter.component';
+import { CarComponent } from './components/car/car.component';
+import { WelcomePageComponent } from './components/welcome-page/welcome-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component:WelcomePageComponent},
+  {path:"cars", component:CarFilterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NaviComponent } from './components/navi/navi.component';
+import { CarComponent } from './components/car/car.component';
+import { CarDetailComponent } from './components/car/car-detail/car-detail.component';
+import { CarFilterComponent } from './components/car/car-filter/car-filter.component';
+import { WelcomePageComponent } from './components/welcome-page/welcome-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NaviComponent,
+    CarComponent,
+    CarDetailComponent,
+    CarFilterComponent,
+    WelcomePageComponent
   ],
   imports: [
     BrowserModule,
